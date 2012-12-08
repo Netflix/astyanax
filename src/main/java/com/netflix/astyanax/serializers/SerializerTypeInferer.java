6b06c07c81f178d6f4ba66f20df310992ad43a36
elandau <elandau@yahoo.com>
2012-12-07 17:02:37 -0800

    Update SerializerTypeInferer for new types

--- src/main/java/com/netflix/astyanax/serializers/SerializerTypeInferer.java
+++ src/main/java/com/netflix/astyanax/serializers/SerializerTypeInferer.java
@@ -32,10 +32,16 @@ public class SerializerTypeInferer {
             serializer = LongSerializer.get();
         }
         else if (value instanceof Integer) {
-            serializer = IntegerSerializer.get();
+            serializer = Int32Serializer.get();
         }
         else if (value instanceof Short) {
             serializer = ShortSerializer.get();
+        }
+        else if (value instanceof Float) {
+            serializer = FloatSerializer.get();
+        }
+        else if (value instanceof Byte) {
+            serializer = ByteSerializer.get();
         }
         else if (value instanceof BigInteger) {
             serializer = BigIntegerSerializer.get();
@@ -73,10 +79,16 @@ public class SerializerTypeInferer {
             serializer = LongSerializer.get();
         }
         else if (valueClass.equals(Integer.class) || valueClass.equals(int.class)) {
-            serializer = IntegerSerializer.get();
+            serializer = Int32Serializer.get();
         }
         else if (valueClass.equals(Short.class) || valueClass.equals(short.class)) {
             serializer = ShortSerializer.get();
+        }
+        else if (valueClass.equals(Float.class) || valueClass.equals(float.class)) {
+            serializer = FloatSerializer.get();
+        }
+        else if (valueClass.equals(Byte.class) || valueClass.equals(byte.class)) {
+            serializer = ByteSerializer.get();
         }
         else if (valueClass.equals(Boolean.class) || valueClass.equals(boolean.class)) {
             serializer = BooleanSerializer.get();

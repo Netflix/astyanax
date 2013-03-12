ca4fcf0d82dc94d8414ce6b562c7497b0e2c1e57
msimonsen <msimonsen@blackberry.com>
2013-03-12 14:48:52 -0400

    catch the exception in drop - might want to consider just catching invalid request

--- src/main/java/com/netflix/astyanax/index/SchemaIndexUtil.java
+++ src/main/java/com/netflix/astyanax/index/SchemaIndexUtil.java
@@ -3,6 +3,9 @@ package com.netflix.astyanax.index;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.ImmutableMap;
 import com.netflix.astyanax.Keyspace;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
@@ -14,6 +17,8 @@ public class SchemaIndexUtil {
 	public static String CACHE_KEY = "caching";
 	public static String CACHE_VAL = "ALL";
 	
+	private static Logger LOG = LoggerFactory.getLogger(SchemaIndexUtil.class);
+	 
 	public static void createIndexCF(Keyspace keyspace,String CFName,boolean drop,boolean cacheAll) throws ConnectionException  {
 		
 		ColumnFamily<byte[], byte[]> index_cf = ColumnFamily.newColumnFamily(
@@ -22,8 +27,11 @@ public class SchemaIndexUtil {
 		
 		//DROP
 		if (drop) {
-			keyspace.dropColumnFamily(index_cf);
-		
+			try {
+				keyspace.dropColumnFamily(index_cf);
+			}catch (Exception e) {
+				LOG.error(e.getMessage(), e);
+			}
 		}
 		//CREATE
 		if (cacheAll)

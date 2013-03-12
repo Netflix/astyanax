9430ecb0a9c319f7ca2a6216bc88ed6f31edaa07
msimonsen <msimonsen@blackberry.com>
2013-03-12 09:46:16 -0400

    javadoc

--- src/main/java/com/netflix/astyanax/index/IndexCoordination.java
+++ src/main/java/com/netflix/astyanax/index/IndexCoordination.java
@@ -30,34 +30,98 @@ public interface IndexCoordination {
 	
 	<C,K> IndexMetadata<C, K> getMetaData(IndexMappingKey<C> key);
 	
+	/**
+	 * A check to see if we have a local copy of the index meta data {@link IndexMetadata}
+	 * via the key
+	 * @param key
+	 * @return
+	 */
 	<C> boolean indexExists(IndexMappingKey<C> key);
 	
+	/**
+	 * Same as above method - exposing the index mapping key as composite of it's primitives.
+	 * @param cf
+	 * @param columnName
+	 * @return
+	 */
 	<C> boolean indexExists(String cf, C columnName);
 	
+	/**
+	 * A column family provided can have multiple {@link IndexMetadata}
+	 * hence returns a list.
+	 * 
+	 * @param cf
+	 * @return
+	 */
 	<C,K> List<IndexMetadata<C, K>> getMetaDataByCf(String cf);
 	
-	
+	/**
+	 * Called internally so that the coordinator can keep track of 
+	 * what is being read currently by the index mapping in question.
+	 * 
+	 * @param mapping
+	 * @throws NoMetaDataException
+	 */
 	<C,V> void reading(IndexMapping<C,V> mapping) throws NoMetaDataException;
 	
+	/**
+	 * Same as above - consider removing.
+	 * @param cf
+	 * @param columnName
+	 * @param value
+	 * @throws NoMetaDataException
+	 */
 	<C,V> void reading(String cf,C columnName,V value) throws NoMetaDataException;
 	
-	
+	/**
+	 * Gets an index mapping by it's key - used internally
+	 * @param key
+	 * @return
+	 */
 	<C,V> IndexMapping<C,V> get(IndexMappingKey<C> key);
 		
 	
-	
+	/**
+	 * Indicates that a value is mapped by the key is being modified.
+	 * Used internally.
+	 * 
+	 * @param key
+	 * @param newValue
+	 * @return
+	 * @throws NoReadException
+	 */
 	<C,V> IndexMapping<C,V> modifying(IndexMappingKey<C> key, V newValue) throws NoReadException;
 	
-	
+	/**
+	 * Same as above - used internally.
+	 * @param cf
+	 * @param columnName
+	 * @param newValue
+	 * @return
+	 * @throws NoReadException
+	 */
 	<C,V> IndexMapping<C,V> modifying(String cf, C columnName, V newValue) throws NoReadException;
 	
 	
 	
-	//checked or unchecked, that is the question!!
+	//These exceptions are currently under consideration
+	//but the premise is that they will be thrown when reading a 
+	//column family that has no meta data associated with it.
+	
 	public class NoReadException extends RuntimeException {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3364833516740899944L;
 		
 	}
 	public class NoMetaDataException extends RuntimeException {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 126548100391024530L;
 		
 	}
 }

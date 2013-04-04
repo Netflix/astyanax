514b8d7d88ef4426b94d9b72c5655a4cec028c62
Shyam Singh <shyam.singh@outlook.com>
2013-04-04 14:33:57 -0700

    fixing the tab spacing

--- astyanax-thrift/src/test/java/com/netflix/astyanax/thrift/CqlTest.java
+++ astyanax-thrift/src/test/java/com/netflix/astyanax/thrift/CqlTest.java
@@ -41,38 +41,33 @@ public class CqlTest {
 
     private static Logger LOG = LoggerFactory.getLogger(CqlTest.class);
 
-    private static Keyspace                  keyspace;
+    private static Keyspace keyspace;
     private static AstyanaxContext<Keyspace> keyspaceContext;
 
-    private static String TEST_CLUSTER_NAME  = "cass_sandbox";
+    private static String TEST_CLUSTER_NAME = "cass_sandbox";
     private static String TEST_KEYSPACE_NAME = "CqlTest";
 
     private static final String SEEDS = "localhost:9160";
 
-    private static final long   CASSANDRA_WAIT_TIME = 1000;
-    private static final int    TTL                 = 20;
-    private static final int    TIMEOUT             = 10;
-    
-    static ColumnFamily<Integer, String> CQL3_CF = ColumnFamily.newColumnFamily(
-            "Cql3CF", 
-            IntegerSerializer.get(), 
-            StringSerializer.get());
-    
+    private static final long CASSANDRA_WAIT_TIME = 1000;
+    private static final int TTL = 20;
+    private static final int TIMEOUT = 10;
+
+    static ColumnFamily<Integer, String> CQL3_CF = ColumnFamily
+            .newColumnFamily("Cql3CF", IntegerSerializer.get(),
+                    StringSerializer.get());
+
     static ColumnFamily<String, String> User_CF = ColumnFamily.newColumnFamily(
-            "UserCF", 
-            StringSerializer.get(), 
-            StringSerializer.get());
+            "UserCF", StringSerializer.get(), StringSerializer.get());
     static ColumnFamily<UUID, String> UUID_CF = ColumnFamily.newColumnFamily(
-            "uuidtest", 
-            UUIDSerializer.get(), 
-            StringSerializer.get());
-    
+            "uuidtest", UUIDSerializer.get(), StringSerializer.get());
+
     @BeforeClass
     public static void setup() throws Exception {
         SingletonEmbeddedCassandra.getInstance();
-        
+
         Thread.sleep(CASSANDRA_WAIT_TIME);
-        
+
         createKeyspace();
     }
 
@@ -80,7 +75,7 @@ public class CqlTest {
     public static void teardown() throws Exception {
         if (keyspaceContext != null)
             keyspaceContext.shutdown();
-        
+
         Thread.sleep(CASSANDRA_WAIT_TIME);
     }
 
@@ -90,81 +85,86 @@ public class CqlTest {
                 .forKeyspace(TEST_KEYSPACE_NAME)
                 .withAstyanaxConfiguration(
                         new AstyanaxConfigurationImpl()
-                                .setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE)
-                                .setConnectionPoolType(ConnectionPoolType.TOKEN_AWARE)
+                                .setDiscoveryType(
+                                        NodeDiscoveryType.RING_DESCRIBE)
+                                .setConnectionPoolType(
+                                        ConnectionPoolType.TOKEN_AWARE)
                                 .setDiscoveryDelayInSeconds(60000)
                                 .setTargetCassandraVersion("1.2")
-                                .setCqlVersion("3.0.0")
-                                )
+                                .setCqlVersion("3.0.0"))
                 .withConnectionPoolConfiguration(
                         new ConnectionPoolConfigurationImpl(TEST_CLUSTER_NAME
                                 + "_" + TEST_KEYSPACE_NAME)
                                 .setSocketTimeout(30000)
                                 .setMaxTimeoutWhenExhausted(2000)
-                                .setMaxConnsPerHost(10)
-                                .setInitConnsPerHost(10)
+                                .setMaxConnsPerHost(10).setInitConnsPerHost(10)
                                 .setSeeds(SEEDS))
                 .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
                 .buildKeyspace(ThriftFamilyFactory.getInstance());
 
         keyspaceContext.start();
-        
+
         keyspace = keyspaceContext.getEntity();
-        
+
         try {
             keyspace.dropKeyspace();
             Thread.sleep(CASSANDRA_WAIT_TIME);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOG.info("Error dropping keyspace " + e.getMessage());
         }
-        
-        keyspace.createKeyspace(ImmutableMap.<String, Object>builder()
-                .put("strategy_options", ImmutableMap.<String, Object>builder()
-                        .put("replication_factor", "1")
-                        .build())
-                .put("strategy_class",     "SimpleStrategy")
-                .build()
-                );
-        
+
+        keyspace.createKeyspace(ImmutableMap
+                .<String, Object> builder()
+                .put("strategy_options",
+                        ImmutableMap.<String, Object> builder()
+                                .put("replication_factor", "1").build())
+                .put("strategy_class", "SimpleStrategy").build());
+
         Thread.sleep(CASSANDRA_WAIT_TIME);
-        
+
         OperationResult<CqlStatementResult> result;
 
-        result = keyspace.prepareCqlStatement()
-            .withCql("CREATE TABLE employees (empID int, deptID int, first_name varchar, last_name varchar, PRIMARY KEY (empID, deptID));")
-            .execute();
-        
-        result = keyspace.prepareCqlStatement()
-            .withCql("CREATE TABLE users (id text PRIMARY KEY, given text, surname text, favs map<text, text>);")
-            .execute();
-        
-        result = keyspace.prepareCqlStatement()
-                .withCql("CREATE TABLE uuidtest (id UUID PRIMARY KEY, given text, surname text);")
+        result = keyspace
+                .prepareCqlStatement()
+                .withCql(
+                        "CREATE TABLE employees (empID int, deptID int, first_name varchar, last_name varchar, PRIMARY KEY (empID, deptID));")
                 .execute();
-        
+
+        result = keyspace
+                .prepareCqlStatement()
+                .withCql(
+                        "CREATE TABLE users (id text PRIMARY KEY, given text, surname text, favs map<text, text>);")
+                .execute();
+
+        result = keyspace
+                .prepareCqlStatement()
+                .withCql(
+                        "CREATE TABLE uuidtest (id UUID PRIMARY KEY, given text, surname text);")
+                .execute();
+
         Thread.sleep(CASSANDRA_WAIT_TIME);
-        
+
         KeyspaceDefinition ki = keyspaceContext.getEntity().describeKeyspace();
         LOG.info("Describe Keyspace: " + ki.getName());
-        
+
     }
-    
+
     @Test
     public void testCompoundKey() throws Exception {
         OperationResult<CqlStatementResult> result;
         result = keyspace
                 .prepareCqlStatement()
-                .withCql("INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('111', '222', 'eran', 'landau');")
+                .withCql(
+                        "INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('111', '222', 'eran', 'landau');")
                 .execute();
-        
+
         result = keyspace
                 .prepareCqlStatement()
-                .withCql("INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('111', '233', 'netta', 'landau');")
+                .withCql(
+                        "INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('111', '233', 'netta', 'landau');")
                 .execute();
-        
-        result = keyspace
-                .prepareCqlStatement()
+
+        result = keyspace.prepareCqlStatement()
                 .withCql("SELECT * FROM employees WHERE empId='111';")
                 .execute();
 
@@ -173,32 +173,30 @@ public class CqlTest {
             LOG.info("CQL Key: " + row.getKey());
 
             ColumnList<String> columns = row.getColumns();
-            
-            LOG.info("   empid      : " + columns.getIntegerValue("empid",      null));
-            LOG.info("   deptid     : " + columns.getIntegerValue("deptid",     null));
-            LOG.info("   first_name : " + columns.getStringValue ("first_name", null));
-            LOG.info("   last_name  : " + columns.getStringValue ("last_name",  null));
-        }   
+
+            LOG.info("   empid      : "
+                    + columns.getIntegerValue("empid", null));
+            LOG.info("   deptid     : "
+                    + columns.getIntegerValue("deptid", null));
+            LOG.info("   first_name : "
+                    + columns.getStringValue("first_name", null));
+            LOG.info("   last_name  : "
+                    + columns.getStringValue("last_name", null));
+        }
     }
-    
+
     @Test
     public void testPreparedCql() throws Exception {
         OperationResult<CqlResult<Integer, String>> result;
-        
+
         final String INSERT_STATEMENT = "INSERT INTO employees (empID, deptID, first_name, last_name) VALUES (?, ?, ?, ?);";
-        
-        result = keyspace
-                .prepareQuery(CQL3_CF)
-                    .withCql(INSERT_STATEMENT)
-                .asPreparedStatement()
-                    .withIntegerValue(222)
-                    .withIntegerValue(333)
-                    .withStringValue("Netta")
-                    .withStringValue("Landau")
-                .execute();
-        
-        result = keyspace
-                .prepareQuery(CQL3_CF)
+
+        result = keyspace.prepareQuery(CQL3_CF).withCql(INSERT_STATEMENT)
+                .asPreparedStatement().withIntegerValue(222)
+                .withIntegerValue(333).withStringValue("Netta")
+                .withStringValue("Landau").execute();
+
+        result = keyspace.prepareQuery(CQL3_CF)
                 .withCql("SELECT * FROM employees WHERE empId='222';")
                 .execute();
         Assert.assertTrue(!result.getResult().getRows().isEmpty());
@@ -206,31 +204,35 @@ public class CqlTest {
             LOG.info("CQL Key: " + row.getKey());
 
             ColumnList<String> columns = row.getColumns();
-            
-            LOG.info("   empid      : " + columns.getIntegerValue("empid",      null));
-            LOG.info("   deptid     : " + columns.getIntegerValue("deptid",     null));
-            LOG.info("   first_name : " + columns.getStringValue ("first_name", null));
-            LOG.info("   last_name  : " + columns.getStringValue ("last_name",  null));
-        }           
+
+            LOG.info("   empid      : "
+                    + columns.getIntegerValue("empid", null));
+            LOG.info("   deptid     : "
+                    + columns.getIntegerValue("deptid", null));
+            LOG.info("   first_name : "
+                    + columns.getStringValue("first_name", null));
+            LOG.info("   last_name  : "
+                    + columns.getStringValue("last_name", null));
+        }
     }
-    
+
     @Test
     public void testKeyspaceCql() throws Exception {
         keyspace.prepareQuery(CQL3_CF)
-                .withCql("INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('999', '233', 'arielle', 'landau');")
+                .withCql(
+                        "INSERT INTO employees (empID, deptID, first_name, last_name) VALUES ('999', '233', 'arielle', 'landau');")
                 .execute();
-        
+
         CqlStatementResult result = keyspace.prepareCqlStatement()
                 .withCql("SELECT * FROM employees WHERE empID = '999';")
-                .execute()
-                .getResult();
-        
+                .execute().getResult();
+
         CqlSchema schema = result.getSchema();
         Rows<Integer, String> rows = result.getRows(CQL3_CF);
-        
-        Assert.assertEquals(1,  rows.size());
-//        Assert.assertTrue(999 == rows.getRowByIndex(0).getKey());
-        
+
+        Assert.assertEquals(1, rows.size());
+        // Assert.assertTrue(999 == rows.getRowByIndex(0).getKey());
+
     }
 
     @Test
@@ -238,17 +240,17 @@ public class CqlTest {
         OperationResult<CqlStatementResult> result;
         result = keyspace
                 .prepareCqlStatement()
-                .withCql("INSERT INTO users (id, given, surname, favs) VALUES ('jsmith', 'John', 'Smith', { 'fruit' : 'apple', 'band' : 'Beatles' })")
+                .withCql(
+                        "INSERT INTO users (id, given, surname, favs) VALUES ('jsmith', 'John', 'Smith', { 'fruit' : 'apple', 'band' : 'Beatles' })")
                 .execute();
 
         Rows<String, String> rows = keyspace.prepareCqlStatement()
-                .withCql("SELECT * FROM users;")
-                .execute()
-                .getResult()
+                .withCql("SELECT * FROM users;").execute().getResult()
                 .getRows(User_CF);
-        
-        MapSerializer<String, String> mapSerializer = new MapSerializer<String, String>(UTF8Type.instance, UTF8Type.instance);
-        
+
+        MapSerializer<String, String> mapSerializer = new MapSerializer<String, String>(
+                UTF8Type.instance, UTF8Type.instance);
+
         for (Row<String, String> row : rows) {
             LOG.info(row.getKey());
             for (Column<String> column : row.getColumns()) {
@@ -261,74 +263,73 @@ public class CqlTest {
             }
         }
     }
+
     @Test
-    public  void testUUID() throws Exception{
-    	CqlStatementResult result = keyspace.prepareCqlStatement()
-    	        .withCql("SELECT * FROM uuidtest ;")
-    	        .execute()
-    	        .getResult();
-    	OperationResult<CqlResult<UUID,String>>  res1 =keyspace.prepareQuery(UUID_CF)
-        .withCql("INSERT INTO uuidtest (id, given, surname) VALUES (00000000-0000-0000-0000-000000000000, 'x', 'arielle');")
-        .execute();
-    	CqlResult<UUID,String> res2 = res1.getResult();
-//    	boolean b = res2.hasRows();
-//    	int num = res2.getNumber();
+    public void testUUID() throws Exception {
+        CqlStatementResult result = keyspace.prepareCqlStatement()
+                .withCql("SELECT * FROM uuidtest ;").execute().getResult();
+        OperationResult<CqlResult<UUID, String>> res1 = keyspace
+                .prepareQuery(UUID_CF)
+                .withCql(
+                        "INSERT INTO uuidtest (id, given, surname) VALUES (00000000-0000-0000-0000-000000000000, 'x', 'arielle');")
+                .execute();
+        CqlResult<UUID, String> res2 = res1.getResult();
+        // boolean b = res2.hasRows();
+        // int num = res2.getNumber();
 
-    	result = keyspace.prepareCqlStatement()
-        .withCql("SELECT * FROM uuidtest ;")
-        .execute()
-        .getResult();
-    	String b = result.toString();
+        result = keyspace.prepareCqlStatement()
+                .withCql("SELECT * FROM uuidtest ;").execute().getResult();
+        String b = result.toString();
 
-    	CqlSchema schema = result.getSchema();
-    	Rows<UUID, String> rows = result.getRows(UUID_CF);
+        CqlSchema schema = result.getSchema();
+        Rows<UUID, String> rows = result.getRows(UUID_CF);
 
-    	Assert.assertEquals(1,  rows.size());
-//		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
-//				.forCluster("Test Cluster")
-//				.forKeyspace("grd")
-//				.withAstyanaxConfiguration(
-//						new AstyanaxConfigurationImpl()
-//								.setDiscoveryType(
-//										NodeDiscoveryType.RING_DESCRIBE)
-//								.setCqlVersion("3.0.0")
-//								.setTargetCassandraVersion("1.2.3"))
-//				// TODO: set both connectionTimeout and readTimeout
-//				.withConnectionPoolConfiguration(
-//						new ConnectionPoolConfigurationImpl("MyConnectionPool")
-//								.setPort(50825).setMaxConnsPerHost(10)
-//								.setSeeds("localhost:9160")
-//								.setConnectTimeout(20000))
-//				.withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
-//				.buildKeyspace(ThriftFamilyFactory.getInstance());
-//
-//		context.start();
-//
-//		// logger.log(Level.INFO, "getting context.. done ");
-//		Keyspace keyspace = context.getEntity();
-//
-//		ColumnFamily<UUID, String> routeCF = new ColumnFamily<UUID, String>(
-//				"grd.test1", (Serializer<UUID>) UUIDSerializer.get(),
-//				(Serializer<String>) StringSerializer.get());
+        Assert.assertEquals(1, rows.size());
+        // AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
+        // .forCluster("Test Cluster")
+        // .forKeyspace("grd")
+        // .withAstyanaxConfiguration(
+        // new AstyanaxConfigurationImpl()
+        // .setDiscoveryType(
+        // NodeDiscoveryType.RING_DESCRIBE)
+        // .setCqlVersion("3.0.0")
+        // .setTargetCassandraVersion("1.2.3"))
+        // // TODO: set both connectionTimeout and readTimeout
+        // .withConnectionPoolConfiguration(
+        // new ConnectionPoolConfigurationImpl("MyConnectionPool")
+        // .setPort(50825).setMaxConnsPerHost(10)
+        // .setSeeds("localhost:9160")
+        // .setConnectTimeout(20000))
+        // .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
+        // .buildKeyspace(ThriftFamilyFactory.getInstance());
+        //
+        // context.start();
+        //
+        // // logger.log(Level.INFO, "getting context.. done ");
+        // Keyspace keyspace = context.getEntity();
+        //
+        // ColumnFamily<UUID, String> routeCF = new ColumnFamily<UUID, String>(
+        // "grd.test1", (Serializer<UUID>) UUIDSerializer.get(),
+        // (Serializer<String>) StringSerializer.get());
 
-//		OperationResult<CqlResult<UUID, String>> result;
-//		try {
-//			result = keyspace.prepareQuery(routeCF)
-//					.withCql("select * from grd.test1;").execute();
-//			for (Row<UUID, String> row : result.getResult().getRows()) {
-//				System.out.println("Row Key: " + row.getKey());
-//				ColumnList<String> columns = row.getColumns();
-//				System.out.println("col1: "
-//						+ columns.getUUIDValue("col1", null));
-//				System.out.println("col2: "
-//						+ columns.getStringValue("col2", null));
-//				System.out.println("col3: "
-//						+ columns.getStringValue("col3", null));
-//			}
-//		} catch (ConnectionException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+        // OperationResult<CqlResult<UUID, String>> result;
+        // try {
+        // result = keyspace.prepareQuery(routeCF)
+        // .withCql("select * from grd.test1;").execute();
+        // for (Row<UUID, String> row : result.getResult().getRows()) {
+        // System.out.println("Row Key: " + row.getKey());
+        // ColumnList<String> columns = row.getColumns();
+        // System.out.println("col1: "
+        // + columns.getUUIDValue("col1", null));
+        // System.out.println("col2: "
+        // + columns.getStringValue("col2", null));
+        // System.out.println("col3: "
+        // + columns.getStringValue("col3", null));
+        // }
+        // } catch (ConnectionException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
 
-	}
+    }
 }

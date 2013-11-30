f1152e992a278d00dfd5007842c049a21ec38cfc
Yeh-Yung <yyc1217@gmail.com>
2013-11-30 12:24:13 +0800

    if type of field is Map or Set, then get serializer from its declared generic type

--- astyanax-entity-mapper/src/main/java/com/netflix/astyanax/entitystore/MappingUtils.java
+++ astyanax-entity-mapper/src/main/java/com/netflix/astyanax/entitystore/MappingUtils.java
@@ -2,6 +2,9 @@ package com.netflix.astyanax.entitystore;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
 
@@ -10,36 +13,65 @@ import org.apache.commons.lang.StringUtils;
 import com.netflix.astyanax.serializers.SerializerTypeInferer;
 
 public class MappingUtils {
+	
     static com.netflix.astyanax.Serializer<?> getSerializerForField(Field field) {
         com.netflix.astyanax.Serializer<?> serializer = null;
+        
         // check if there is explicit @Serializer annotation first
-        Serializer serializerAnnotation = field.getAnnotation(Serializer.class);
-        if(serializerAnnotation != null) {
-            final Class<?> serializerClazz = serializerAnnotation.value();
-            // check type
-            if(!(com.netflix.astyanax.Serializer.class.isAssignableFrom(serializerClazz)))
-                throw new RuntimeException("annotated serializer class is not a subclass of com.netflix.astyanax.Serializer. " + serializerClazz.getCanonicalName());
-            // invoke public static get() method
-            try {
-                Method getInstanceMethod = serializerClazz.getMethod("get");
-                serializer = (com.netflix.astyanax.Serializer<?>) getInstanceMethod.invoke(null);
-            } catch(Exception e) {
-                throw new RuntimeException("Failed to get or invoke public static get() method", e);
-            }
-        } else {
-            // otherwise automatically infer the Serializer type from field object type
-            serializer = SerializerTypeInferer.getSerializer(field.getType());
-        }
+		if (hasSerializerAnnotation(field)) {
+			serializer = retrieveSerializerFromAnnotation(field);
+		} else {
+			
+			Class<?> type;
+			if (isMapOrSet(field.getType())) {
+				type = getGenericTypeClass(field);
+			} else {
+				type = field.getType();
+			}
+			serializer = SerializerTypeInferer.getSerializer(type);
+		
+		}
         return serializer;
     }
 
-    static String getEntityName(Entity entityAnnotation, Class<?> clazz) {
+    private static com.netflix.astyanax.Serializer<?> retrieveSerializerFromAnnotation(Field field) {
+    	Serializer serializerAnnotation = field.getAnnotation(Serializer.class);
+        final Class<?> serializerClazz = serializerAnnotation.value();
+        
+        // check type
+        if(!(com.netflix.astyanax.Serializer.class.isAssignableFrom(serializerClazz)))
+            throw new RuntimeException("annotated serializer class is not a subclass of com.netflix.astyanax.Serializer. " + serializerClazz.getCanonicalName());
+        
+        // invoke public static get() method
+        try {
+            Method getInstanceMethod = serializerClazz.getMethod("get");
+            return (com.netflix.astyanax.Serializer<?>) getInstanceMethod.invoke(null);
+        } catch(Exception e) {
+            throw new RuntimeException("Failed to get or invoke public static get() method", e);
+        }
+	}
+
+	private static boolean hasSerializerAnnotation(Field field) {
+    	return field.getAnnotation(Serializer.class)!=null;
+	}
+
+	private static boolean isMapOrSet(Class<?> type) {
+    	boolean result = Map.class.isAssignableFrom(type);
+		result |= Set.class.isAssignableFrom(type);
+		
+		return result;
+	}
+    
+    static Class<?> getGenericTypeClass(Field field){
+        ParameterizedType stringListType = (ParameterizedType) field.getGenericType();
+        return (Class<?>) stringListType.getActualTypeArguments()[0];
+    }
+    
+	static String getEntityName(Entity entityAnnotation, Class<?> clazz) {
         String name = entityAnnotation.name();
         if (name == null || name.isEmpty()) 
             return StringUtils.substringAfterLast(clazz.getName(), ".").toLowerCase();
         else
             return name;
     }
-
-
 }

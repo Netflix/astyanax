f2c3ffc286fa6c198459103e65aa55dc3efa117d
Todd Nine <tnine@apigee.com>
2013-12-09 17:09:56 -0700

    Added test to prove issue

--- astyanax-cassandra/src/test/java/com/netflix/astyanax/model/CompositeTest.java
+++ astyanax-cassandra/src/test/java/com/netflix/astyanax/model/CompositeTest.java
@@ -1,26 +1,63 @@
 package com.netflix.astyanax.model;
 
+
 import java.nio.ByteBuffer;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.netflix.astyanax.serializers.ByteBufferOutputStream;
 
 import junit.framework.Assert;
 
-import org.apache.commons.lang.StringUtils;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import com.netflix.astyanax.serializers.ByteBufferOutputStream;
 
 public class CompositeTest {
     @Test
     public void testByteBufferOutputStream() throws Exception {
         ByteBufferOutputStream out = new ByteBufferOutputStream();
-        
+
         int length = 0;
-        for (int i = 0; i < 300; i++) {
+        for ( int i = 0; i < 300; i++ ) {
             length += i;
-            out.write(StringUtils.repeat("*",  i).getBytes());
+            out.write( StringUtils.repeat( "*", i ).getBytes() );
         }
-        
+
         ByteBuffer buffer = out.getByteBuffer();
-        Assert.assertEquals(buffer.capacity(), length);
+        Assert.assertEquals( buffer.capacity(), length );
+    }
+
+
+    @Test
+    public void compositeSerializesPrimitives() {
+        final CompositeBuilder builder = Composites.newCompositeBuilder();
+
+        final String string = "test";
+        final UUID uuid = UUID.randomUUID();
+        final boolean bool = true;
+        final Integer integer = 10;
+        final Long longval = 20l;
+
+
+        builder.addString( string );
+        builder.addUUID( uuid );
+        builder.addBoolean( bool );
+        builder.addInteger( integer );
+        builder.addLong( longval );
+
+
+        final CompositeParser parser = Composites.newCompositeParser( builder.build() );
+
+        //now read back
+        assertEquals( string, parser.readString() );
+        assertEquals( uuid, parser.readUUID() );
+        assertEquals( bool, parser.readBoolean() );
+        assertEquals( integer, parser.readInteger() );
+        assertEquals( longval, parser.readLong() );
+        assertEquals( string, parser.readString() );
     }
 }
